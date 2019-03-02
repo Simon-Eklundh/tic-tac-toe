693f1b376d4222207b8a297654a33a95521397f2
Simon Eklundh <simon.eklundh@gmail.com>
2019-03-02 16:42:30 +0100

    Update presets.c

--- presets.c
+++ presets.c
@@ -10,19 +10,19 @@ void preset_gameboard(void){
 	display_string(1, "tic tac toe");
 	display_string(2, "mate");
 	display_update();
-	//delay(2000);
+	delay(2000);
 	
 	display_string(0, "Instructions:");
 	display_string(1, "swts fr left=row");
 	display_string(2, "btns fr left=pos");
 	display_update();
-	//delay(6000);
+	delay(4000);
 	
 	display_string(0, "press reset");
 	display_string(1, "button to");
 	display_string(2, "replay");
 	display_update();
-	//delay(2000);
+	delay(2000);
 	
 	display_string( 0 , "|_|_|_|_|");
 	display_string( 1 , "|_|_|_|_|");
